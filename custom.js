6700b906fdb5b2894c09a3d5f628d90caf241f18
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2023-02-25 15:48:49 +0900

    [add] モーダルウィンドウ追加
    * とりあえず動いたサンプル2ベースで

--- custom.js
+++ custom.js
@@ -1,8 +1,23 @@
 jQuery(function () {
-    // デフォルトでは隠しておく
+	// アコーディオンメニュー
     $('.hidden').hide();
-    // nav-open直下のdiv要素をクリックで表示、非表示
     $('.nav-open').click(function () {
         $(this).next('div').slideToggle();
+    });
+
+	// モーダルウィンドウ
+    const modal = $("#js-modal");
+    const overlay = $("#js-overlay");
+    const close = $("#js-close");
+    const open = $("#js-open");
+
+    open.on('click', function () {
+        modal.addClass("open");
+        overlay.addClass("open");
+    });
+
+    close.on('click', function () {
+        modal.removeClass("open");
+        overlay.removeClass("open");
     });
 });
