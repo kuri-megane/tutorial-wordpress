8db2486337d79e8903faf7c6e60326889fb37aca
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2023-03-20 23:27:18 +0900

    [work] 本番サイトでの作業内容を反映
    * モーダルを4つに

--- astra-child/functions.php
+++ astra-child/functions.php
@@ -10,11 +10,11 @@
 
 function enqueue_scripts()
 {
-    // css
+    // css 
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 
-    // js
+    // js 
     wp_enqueue_script('jquery-js', '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
     wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/up-overlay.js');
 }
--- astra-child/up-overlay.js
+++ astra-child/up-overlay.js
@@ -7,23 +7,57 @@
  */
 
 jQuery(function () {
-    $('.hidden').hide();
-    $('.nav-open').click(function () {
-        $(this).next('div').slideToggle();
+        
+    // TODO: 要リファクタ
+    const open_1 = $("#js-open-1");
+    const overlay_1 = $("#js-overlay-1");
+    const modal_1 = $("#js-modal-1");
+    const close_1 = $("#js-close-1");
+    open_1.on('click', function () {
+        modal_1.addClass("open");
+        overlay_1.addClass("open");
+    });
+    close_1.on('click', function () {
+        modal_1.removeClass("open");
+        overlay_1.removeClass("open");
     });
 
-    const modal = $("#js-modal-1");
-    const overlay = $("#js-overlay-1");
-    const close = $("#js-close-1");
-    const open = $("#js-open-1");
-
-    open.on('click', function () {
-        modal.addClass("open");
-        overlay.addClass("open");
+    const open_2 = $("#js-open-2");
+    const overlay_2 = $("#js-overlay-2");
+    const modal_2 = $("#js-modal-2");
+    const close_2 = $("#js-close-2");
+    open_2.on('click', function () {
+        modal_2.addClass("open");
+        overlay_2.addClass("open");
+    });
+    close_2.on('click', function () {
+        modal_2.removeClass("open");
+        overlay_2.removeClass("open");
     });
 
-    close.on('click', function () {
-        modal.removeClass("open");
-        overlay.removeClass("open");
+    const open_3 = $("#js-open-3");
+    const overlay_3 = $("#js-overlay-3");
+    const modal_3 = $("#js-modal-3");
+    const close_3 = $("#js-close-3");
+    open_3.on('click', function () {
+        modal_3.addClass("open");
+        overlay_3.addClass("open");
+    });
+    close_3.on('click', function () {
+        modal_3.removeClass("open");
+        overlay_3.removeClass("open");
+    });
+
+    const open_4 = $("#js-open-4");
+    const overlay_4 = $("#js-overlay-4");
+    const modal_4 = $("#js-modal-4");
+    const close_4 = $("#js-close-4");
+    open_4.on('click', function () {
+        modal_4.addClass("open");
+        overlay_4.addClass("open");
+    });
+    close_4.on('click', function () {
+        modal_4.removeClass("open");
+        overlay_4.removeClass("open");
     });
 });
